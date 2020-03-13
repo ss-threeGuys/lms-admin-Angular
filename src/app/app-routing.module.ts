9b6bd7a2ecb11e90ba4c0d46dd6b0408d14716aa
bmasles <burke.masles@smoothstack.com>
2020-03-13 19:22:23 -0400

    Working genre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthorComponent } from "./author/author.component";
 import { BranchTableComponent } from "./components/branch-table/branch-table.component";
+import { GenreComponent } from './components/genre/genre.component';
 const routes: Routes = [
   { path: "admin/author", component: AuthorComponent },
-  { path: "admin/branches", component: BranchTableComponent }
+  { path: "admin/branches", component: BranchTableComponent },
+  { path: "admin/genres", component: GenreComponent}
 ];
 
 @NgModule({
---
+++ src/app/domain/genre.ts
@@ -0,0 +1,4 @@
+export interface Genre {
+    _id?;
+    name?: string;
+}
