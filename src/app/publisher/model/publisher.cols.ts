90bb3ee642b7f3f00d39217457a21f563436932c
Krissada Jindanupajit <krissada.jindanupajit@smoothstack.com>
2020-03-16 02:59:43 -0400

    Sorting

--- src/app/publisher/model/publisher.cols.ts
+++ src/app/publisher/model/publisher.cols.ts
@@ -1,7 +1,7 @@
 
 export default 
 [
-    { field: 'name', header: 'Name', validator: (x) => (x.toString().trim() != '') },
-    { field: 'address', header: 'Address' },
-    { field: 'phone', header: 'Phone' },
+    { field: 'name',    header: 'Name',     type: 'string', validator: (x) => (x.toString().trim() != '') },
+    { field: 'address', header: 'Address',  type: 'string'},
+    { field: 'phone',   header: 'Phone',    type: 'string'},
 ];
